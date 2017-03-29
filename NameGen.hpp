7ac85084c300f19ef6189e3c429ac50487871acc
Willis Hand <willishand@gmail.com>
2017-03-29 13:49:46 -0400

    better documentation

--- NameGen.hpp
+++ NameGen.hpp
@@ -19,11 +19,40 @@ using namespace std;
 
 class NameGen {
  public:
+
+    //! Constructor that takes no arguments but requires 4 files in ./res/ : first.txt, last.txt, place.txt, and team.txt
+    */
     NameGen();
+
+    //! Member function that returns a random first name from firstBank
+    /*!
+      \return std::string
+    */
     string genFirst();
+
+    //! Member function that returns a random first name from lastBank
+    /*!
+      \return std::string
+    */
     string genLast();
+
+    //! Member function that returns a single string containing "<firstname> <lastname>" pulled from genFirst() and genLast().
+    /*!
+      \return std::string
+    */
     string genFull();
+
+    //! Member function that returns a random place name from placeBank.
+    /*!
+      \return std::string
+    */
     string genPlace();
+
+    //! Member function that has two functionalities based on boolean parameter. If withPlace is FALSE, the string returned will only be an element of teamBank. If withPlace is TRUE, the string will be in the format "<placename> <teamname>" with <placename> pulled from genPlace().
+    /*!
+      \param withPlace If true, team name will be appended to a place name. If false, only team name is returned.
+      \return std::string
+    */
     string genTeam(bool withPlace);
 
  private:
